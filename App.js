edd3e2298b70b99bbf5879ad1719221dc4ddbe5c
allyson09 <allyson.frazier09@gmail.com>
2020-12-09 18:29:10 -0600

    Commit after adding ssh key

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hey y'all!</Text>
+      <Text>Heeey y'all!</Text>
       <StatusBar style="auto" />
     </View>
   );
